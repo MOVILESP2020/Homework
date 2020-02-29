be3ef07be6a3835094687cfdd94aeb0863761ab9
Isaac Cabrera <isaaccabrera31@gmail.com>
2020-02-29 13:26:07 -0600

    Init Have, register adapter and open box

--- homework_03/lib/main.dart
+++ homework_03/lib/main.dart
@@ -3,8 +3,21 @@ import 'package:hive/hive.dart';
 import 'package:homework_03/home/home.dart';
 import 'package:path_provider/path_provider.dart' as path_provider;
 
+import 'models/todo_remainder.dart';
+
 void main() async {
-  // TODO: inicializar hive y agregar el adapter
+  // Inicializar antes de crear app
+  WidgetsFlutterBinding.ensureInitialized();
+  // Acceso al local storage
+  final _localStorage = await path_provider.getApplicationDocumentsDirectory();
+  // Inicializar hive
+  Hive.init(_localStorage.path);
+  // Registrar adapter
+  Hive.registerAdapter(TodoRemainderAdapter());
+  // Abrir la caja
+  await Hive.openBox("reminders");
+
+
   runApp(MyApp());
 }
 
@@ -12,7 +25,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tarea 2',
+      title: 'Tarea 3',
       theme: ThemeData(
         primarySwatch: Colors.indigo,
         primaryColor: Colors.grey[50],
