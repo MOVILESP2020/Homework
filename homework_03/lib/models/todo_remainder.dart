4d3aacfbed3f0e2570c9f511e206a68ae81a90c4
Isaac Cabrera <isaaccabrera31@gmail.com>
2020-02-29 13:24:32 -0600

    Update model to run build runner

--- homework_03/lib/models/todo_remainder.dart
+++ homework_03/lib/models/todo_remainder.dart
@@ -1,6 +1,12 @@
-// TODO: convertir en adapter de Hive y utilizar build runner para generar el adapter
+import 'package:hive/hive.dart';
+
+part 'todo_remainder.g.dart';
+
+@HiveType(typeId: 1)
 class TodoRemainder {
+  @HiveField(0)
   final String todoDescription;
+  @HiveField(1)
   final String hour;
 
   TodoRemainder({
